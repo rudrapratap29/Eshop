3b93741d30a80217ebb9cf9552464ce2bc05f946
rudra <adityamzn002021@gmail.com>
2024-01-09 12:20:11 +0530

    third commit

---
+++ store/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import *
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name', 'price', 'category']
+
+
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+
+
+# Register your models here.
+admin.site.register(Product, AdminProduct)
+admin.site.register(Category , AdminCategory)
+admin.site.register(Customer )
+admin.site.register(Order )
+
+
+# Register your models here.
